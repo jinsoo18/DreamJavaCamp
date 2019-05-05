c65ef62c5e43fc25ba8920eb79cd35b1cf6c7f75
학생53 <53@DESKTOP-3OG9MDM>
2019-05-05 14:11:08 +0900

    Chapter02

---
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -0,0 +1,10 @@
+package icehs.science.chapter02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		System.out.println("²ÞÀÇ ÇÐ±³");
+	}
+
+}
